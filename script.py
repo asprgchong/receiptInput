978352c4a35862e350b7608930f02a229cd77afd
Geralyn <geralynchong26@gmail.com>
2024-07-04 02:00:48 -0500

    please

--- script.py
+++ script.py
@@ -7,3 +7,5 @@ img = cv2.imread('/Users/geralynchong/receiptInput/image1.png')
 # Adding custom options
 custom_config = r'--oem 3 --psm 6'
 print(pytesseract.image_to_string(img, config=custom_config))
+
+
