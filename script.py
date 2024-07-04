9c6ceed11a5f99c1be5ea1165774d0870dd44e5c
Geralyn <geralynchong26@gmail.com>
2024-07-04 01:59:45 -0500

    Setup

---
+++ script.py
@@ -0,0 +1,9 @@
+import cv2 
+import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd = r'/opt/homebrew/bin/tesseract'
+img = cv2.imread('/Users/geralynchong/receiptInput/image1.png')
+
+# Adding custom options
+custom_config = r'--oem 3 --psm 6'
+print(pytesseract.image_to_string(img, config=custom_config))
